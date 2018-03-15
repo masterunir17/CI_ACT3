593063fba3d7a6e4fc08dbc420816c3335cdfdf0
server <~ymc.sergioflores@gmail.com>
2018-03-15 12:27:21 +0100

    añadir clases suma y resta

---
+++ src/main/java/com/Suma.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * Clase tonta para poder hacer test de junit con ella
+ * 
+ */
+public class Suma {
+    public double getSuma(double a, double b) {
+        // Se multiplica en vez de sumar a posta, para que los test fallen
+        return a * b;
+    }
+
+    public double incrementa(double a) {
+        return a + 1;
+    }
+}
